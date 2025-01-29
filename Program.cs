2d06906cbc36963993a673cffd5f41acc16eeeb9
Jose Villa <j0542420@amarillocollege.com>
2025-01-29 17:26:12 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations
+            int score = 0;
+            int A = 0;
+            int B = 0;
+            int C = 0;
+            int D = 0;
+            int F = 0;
+
+            //ask the user for the input of the score
+            Console.WriteLine("What is a numeric score (0 to 100)?");
+            //user inputs their score
+            score = Convert.ToInt32(Console.ReadLine());
+
+            switch (score) 
+            {
+                case 1:
+                    Console.WriteLine($"The letter grade for score {score} is : A");
+                    break;
+                case 2:
+                    Console.WriteLine($"The letter grade for score {score} is : B");
+                    break;
+                case 3:
+                    Console.WriteLine($"The letter grade for score {score} is : C");
+                    break;
+                case 4:
+                    Console.WriteLine($"The letter grade for score {score} is : D");
+                    break;
+                case 5:
+                    Console.WriteLine($"The letter grade for score {score} is : F");
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wk3Ex1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wk3Ex1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63ceedf0-59b4-4a4d-9a05-70060d35773f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
