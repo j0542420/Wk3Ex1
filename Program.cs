3274d0dd6742d017efb17114791666a36d6bafcd
Jose Villa <j0542420@amarillocollege.com>
2025-01-31 18:52:00 -0600

    update

--- Program.cs
+++ Program.cs
@@ -11,40 +11,40 @@ namespace Wk3Ex1
         static void Main(string[] args)
         {
             //declarations
-            int score = 0;
-            int A = 0;
-            int B = 0;
-            int C = 0;
-            int D = 0;
-            int F = 0;
+            double score = 0;
+            
 
             //ask the user for the input of the score
             Console.WriteLine("What is a numeric score (0 to 100)?");
             //user inputs their score
-            score = Convert.ToInt32(Console.ReadLine());
+            score = Convert.ToDouble(Console.ReadLine());
 
-            switch (score) 
+            //users score is divided by 10 to 
+            switch (score / 10) 
             {
+                case 0:
                 case 1:
-                    Console.WriteLine($"The letter grade for score {score} is : A");
-                    break;
                 case 2:
-                    Console.WriteLine($"The letter grade for score {score} is : B");
-                    break;
                 case 3:
-                    Console.WriteLine($"The letter grade for score {score} is : C");
-                    break;
                 case 4:
-                    Console.WriteLine($"The letter grade for score {score} is : D");
-                    break;
                 case 5:
                     Console.WriteLine($"The letter grade for score {score} is : F");
                     break;
-
-
-
-
-
+                case 6:
+                    Console.WriteLine($"The letter grade for score {score} is : D");
+                    break;
+                case 7:
+                    Console.WriteLine($"The letter grade for score {score} is : C");
+                    break;
+                case 8:
+                    Console.WriteLine($"The letter grade for score {score} is : B");
+                    break;
+                case 9:
+                    Console.WriteLine($"The letter grade for score {score} is : A");
+                    break;
+                case 10:
+                    Console.WriteLine($"The letter grade for score {score} is : A");
+                    break;
             }
         }
     }
